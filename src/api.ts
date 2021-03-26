94245d820ff5d87bd91eb49c1cb9b8165623caa3
Aleks Maksimow <aleksander.maksimow@phraseapp.com>
2021-03-26 11:45:32 +0100

    Handle request errors + allow endpoint and token via command flag

--- src/api.ts
+++ src/api.ts
@@ -1,50 +1,56 @@
-import { getAuthTokenFromConfig } from "./config.ts";
+import { apiEndpoint, getAuthToken } from "./config.ts";
 import * as log from "https://deno.land/std@0.91.0/log/mod.ts";
 
-export const endpointUrl = (): string => {
-  return "https://api.phrase.com/v2";
-};
+const handleResponseErrors = (response: Response): void => {
+  if(response["status"] === 401) {
+    log.error(response["statusText"]);
+    log.error("Auth token invalid?");
+    Deno.exit();
+  }
+
+  if(response["status"] !== (200 || 201)) {
+    log.error(response["statusText"]);
+  }
+}
 
 export const getResources = async (
   url: string,
 ): Promise<Record<string, unknown>[]> => {
-  const resourceURL = new URL(url, endpointUrl());
-  const authToken = await getAuthTokenFromConfig();
+  const resourceURL = new URL(url, apiEndpoint());
+  const authToken = await getAuthToken();
 
-  const response = await fetch(resourceURL.href, {
+  return await fetch(resourceURL.href, {
     headers: {
       "Authorization": `Bearer ${authToken}`,
     },
-  });
+  }).then(response => {
+    handleResponseErrors(response)
 
-  return response.json();
+    return response.json();
+  }).catch(error => {
+    log.error(error);
+    return {};
+  });
 };
 
 export const createResource = async (
   url: string,
   data: Record<string, unknown>,
 ): Promise<Record<string, unknown>> => {
-  const resourceURL = new URL(url, endpointUrl());
-  const authToken = await getAuthTokenFromConfig();
+  const resourceURL = new URL(url, apiEndpoint());
+  const authToken = await getAuthToken();
 
-  const formData = new FormData();
-  Object.entries(data).forEach((item) => formData.append(item[0], `${item[1]}`));
-
-  try {
-    return await fetch(resourceURL.href, {
-      method: "POST",
-      headers: {
-        "Authorization": `Bearer ${authToken}`,
-      },
-      body: formData,
-    }).then(response => {
-      if (response["status"] !== 201) log.error(response["statusText"]);
-      return response.json();
-    }).catch(error => {
-      log.error(error);
-    });
-  } catch (error) {
+  return await fetch(resourceURL.href, {
+    method: "POST",
+    headers: {
+      "Authorization": `Bearer ${authToken}`,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data),
+  }).then(response => {
+    handleResponseErrors(response)
+    return response.json();
+  }).catch(error => {
     log.error(error);
-    return {}
-  }
+  });
 };
--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,7 @@ import { exists } from "https://deno.land/std@0.90.0/fs/exists.ts";
 import { parse } from "https://deno.land/std@0.91.0/encoding/yaml.ts";
 import * as log from "https://deno.land/std@0.91.0/log/mod.ts";
 import * as path from "https://deno.land/std@0.90.0/path/mod.ts";
+import { parseFlags } from "https://deno.land/x/cliffy/flags/mod.ts";
 
 type PhraseConfigAttributes = {
   access_token?: string;
@@ -54,7 +55,25 @@ export const getPhraseConfig = async (): Promise<PhraseConfig> => {
   Deno.exit();
 };
 
-export const getAuthTokenFromConfig = async (): Promise<string> => {
-  const phraseConfig = await getPhraseConfig();
-  return phraseConfig.config.access_token || "";
+const flags = (): Record<string, unknown> => {
+  const args = parseFlags(Deno.args);
+  return args.flags;
+}
+
+export const getAuthToken = async (): Promise<string> => {
+
+  if (flags().token) {
+    return flags().token as string;
+  } else {
+    const phraseConfig = await getPhraseConfig();
+    return phraseConfig.config.access_token || ""
+  }
+};
+
+export const apiEndpoint = (): string => {
+  if (flags().endpoint) {
+    return flags().endpoint as string;
+  } else {
+    return "https://api.phrase.com/v2";
+  }
 };
